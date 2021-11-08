38a52f98f2255cb7d57cc7088179a5ca578636a9
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2021-11-08 21:58:39 +0000

    scared of commitment

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+message = "some message"
+print(message.upper())
+
+numbers = [1, 3, 6, 9, 22]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(str(num) + " is a nice number")
+        #wat...
